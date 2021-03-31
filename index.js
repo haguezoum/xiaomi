ba70b0e93e4bcd66aa7747b23ad62b8bab2df401
Hassan Aguezoum <haguezoum@gmail.com>
2021-03-31 02:51:06 +0100

    first commit
    #1

---
+++ index.js
@@ -0,0 +1,13 @@
+let newBtn = document.getElementById('new-logo');
+let oldBtn = document.getElementById('old-logo');
+let logo   = document.querySelector('.logo-panel .logo');
+
+newBtn.addEventListener('focus',()=>{
+    logo.style.borderRadius='5%'
+});
+oldBtn.addEventListener('focus',()=>{
+    logo.style.borderRadius='40%'
+});
+ 
+
+
